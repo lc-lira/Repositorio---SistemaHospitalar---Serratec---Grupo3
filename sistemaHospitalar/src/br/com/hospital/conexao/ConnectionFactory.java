f778ac7695e50e7b6b8cf15af01220979698ec57
MarceloMdx <marcelosaphira@gmail.com>
2026-04-20 16:05:16 -0300

    Estrutura inicial e conexão com banco de dados

---
+++ sistemaHospitalar/src/br/com/hospital/conexao/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package br.com.hospital.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private String url = "jdbc:postgresql://ep-cold-sunset-ac3dirxl-pooler.sa-east-1.aws.neon.tech:5432/postgres";
+    private String usuario = "neondb_owner";
+    private String senha = "npg_j0iWzkZYGt3g";
+
+    private Connection connection;
+
+    public Connection getConnection() {
+        System.out.println("Conectando no banco de dados....");
+        try {
+            connection = DriverManager.getConnection(url, usuario, senha);
+            if (connection != null) {
+                System.out.println("Conectado com sucesso!");
+                return connection;
+            } else {
+                System.out.println("Não foi possível conectar!");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Problemas no Driver ou senha incorreta do banco");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
+
---
+++ sistemaHospitalar/src/br/com/hospital/conexao/TesteConexao.java
@@ -0,0 +1,10 @@
+package br.com.hospital.conexao;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+       
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.getConnection();
+    }
+}
+
