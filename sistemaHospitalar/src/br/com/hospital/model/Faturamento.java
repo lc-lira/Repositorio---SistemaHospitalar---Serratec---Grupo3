2a0a07b3e8358769244ef60c64ccb67c159aaf44
Bruno-PMendonca <brunompassamani@gmail.com>
2026-04-20 21:42:49 -0300

    Funcao de trazer os dados funcionando

---
+++ sistemaHospitalar/src/br/com/hospital/model/Faturamento.java
@@ -0,0 +1,39 @@
+package br.com.hospital.model;
+public class Faturamento {
+    private String nome;
+    private Double valor;   
+
+
+    public Faturamento(String nome, Double valor) {
+        this.nome = nome;
+        this.valor = valor;
+        toString();
+    }
+
+    
+    @Override
+    public String toString() {
+            return "Faturamento: nome= " + nome + " , valor= " + valor + '}';
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public Double getValor() {
+        return valor;
+    }
+
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+}
